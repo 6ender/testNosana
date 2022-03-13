5e6d4bf604374525d81e76938e673e526fc443e0
6ender <80064765+6ender@users.noreply.github.com>
2022-03-14 00:28:10 +0300

    Create indexNosana.js
    test

---
+++ indexNosana.js
@@ -0,0 +1,5 @@
+076b385ec3aa21045a9d3269848ba20ec5e3150bf1e6a6a4f9cb940087588b72  guix-build-e857f0bb55b1/output/arm64-apple-darwin/bitcoin-e857f0bb55b1-osx-unsigned.dmg
+9cd50f1fb66b817f76a7dda5db29cab1abe68a8eba5f0192c7e7350ebc160313  guix-build-e857f0bb55b1/output/arm64-apple-darwin/bitcoin-e857f0bb55b1-osx-unsigned.tar.gz
+af674d14f616526de8737cf79ab4f4dff81a9737bebf92fd45ebd17b99b560a1  guix-build-e857f0bb55b1/output/dist-archive/bitcoin-e857f0bb55b1.tar.gz
+ebea43c2fd7f7883055219c99c96bab5b77c82060d5e977de9be9639fe343cd8  guix-build-e857f0bb55b1/output/x86_64-apple-darwin/SHA256SUMS.part
+9d4a93f1a82224b901fabe04081fa15e19692c91b5b53f17af5cab468b1185fe  guix-build-e857f0bb55b1/output/x86_64-apple-darwin/bitcoin-e857f0bb55b1-osx-unsigned.dmg
